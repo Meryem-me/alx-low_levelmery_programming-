094715240396990421bfcdc950086263a089ab03
Meryem-me <saikoukimeryem@gmail.com>
2023-04-05 16:35:41 -0700

    message

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string.
- * @s: String Passed
- * Return: NULL
- */
+* _puts_recursion - prints a string.
+*@s: String Passed
+*Return: NULL
+*/
 
 void _puts_recursion(char *s)
 {
