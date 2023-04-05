38b264812d35f79a9cac1318450a00eab4ba63e4
Meryem-me <saikoukimeryem@gmail.com>
2023-04-05 16:38:14 -0700

    message

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
-#ifndef MAIN.H
+#ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,4 +11,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
+
+
 #endif
