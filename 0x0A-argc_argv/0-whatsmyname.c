718a3d3c49ba0cfd301abae27232572e3a6be317
Meryem-me <saikoukimeryem@gmail.com>
2023-04-11 07:27:26 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
+
 /**
- *
- * main - write a program that prints its name, followed by a new line.
- * If you rename the program, it will print the new name, without having to
- * compile it again
- * You should not remove the path before the name of the program
- *
+ * main - is a function that prints the  name of the file
  * @argc: This is the argument count
  * @argv: This is the argument vector
- *
  * Return: This return to 0
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	(void)argc;
+	printf("%s\n", argv[argc - argc])
 	return (0);
 }
